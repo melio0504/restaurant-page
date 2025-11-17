cd5288b9a433988f8426ed3e065424d2a8dbb043
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-11-17 21:01:36 +0800

    Add html-loader in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,27 +7,30 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    clean: true
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/template.html"]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
-      favicon: "./public/favicon.png",
+      template: "./src/template.html"
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: "asset/resource"
       },
     ],
   },
