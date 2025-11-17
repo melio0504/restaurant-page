2e17c2b18eef2a5329a38a2ad859b3765e55a191
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-11-17 09:14:04 +0800

    Import css and default module from greeting.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Code here
+import './styles.css';
+import greeting from './greeting.js';
+
+document.body.appendChild(greeting());
+
+/* 
+  We can import images file using like this in our module
+*/
+
+// import odinImage from "./odin.png";
+   
+// const image = document.createElement("img");
+// image.src = odinImage;
+   
+// document.body.appendChild(image);
