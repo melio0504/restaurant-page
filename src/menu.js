0fa452271ec6eac191268c9d24a11edc696ad4d3
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-11-21 09:10:24 +0800

    Import all product pictures in menu module

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,10 @@
+import Shokupan from '..public/images/shokupan.png';
+import Anpan from '..public/images/anpan.png';
+import yakisobaPan from '..public/images/yakisoba-pan.png';
+import matchaBread from '..public/images/matcha-bread.png';
+import creamPan from '..public/images/cream-pan.png';
+import Croisant from '..public/images/croisant.png';
+
 const menuPage = () => {
   /* 
     TODO:
